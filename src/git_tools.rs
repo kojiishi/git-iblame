5e9bc08c0fd13722132f693efbb94663416f828a
Koji Ishii <kojii@chromium.org>
2025-04-28 03:39:03 +0900

    Fix style

--- src/git_tools.rs
+++ src/git_tools.rs
@@ -25,28 +25,28 @@ impl GitTools {
     }
 
     pub fn repository(&mut self) -> &mut Repository {
-      &mut self.repository
+        &mut self.repository
     }
 
     pub fn root_path(&self) -> &Path {
-      &self.root_path
+        &self.root_path
     }
 
     pub fn content_as_string(&mut self, commit_id: Oid, path: &Path) -> anyhow::Result<String> {
-      let commit = self.repository.find_commit(commit_id)?;
-      let tree = commit.tree()?;
-      let entry = tree.get_path(path)?;
-      let object = entry.to_object(&self.repository)?;
-      let blob = object.into_blob().unwrap();
-      let content = std::str::from_utf8(blob.content())?.to_string();
-      Ok(content)
-  }
+        let commit = self.repository.find_commit(commit_id)?;
+        let tree = commit.tree()?;
+        let entry = tree.get_path(path)?;
+        let object = entry.to_object(&self.repository)?;
+        let blob = object.into_blob().unwrap();
+        let content = std::str::from_utf8(blob.content())?.to_string();
+        Ok(content)
+    }
 
-  pub fn older_commit_id(&self, id: Oid) -> anyhow::Result<Oid> {
-    let mut revwalk = self.repository.revwalk()?;
-    revwalk.push(id)?;
-    let _ = revwalk.next().unwrap()?;
-    let previous_id = revwalk.next().unwrap()?;
-    Ok(previous_id)
-}
+    pub fn older_commit_id(&self, id: Oid) -> anyhow::Result<Oid> {
+        let mut revwalk = self.repository.revwalk()?;
+        revwalk.push(id)?;
+        let _ = revwalk.next().unwrap()?;
+        let previous_id = revwalk.next().unwrap()?;
+        Ok(previous_id)
+    }
 }
