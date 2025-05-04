ff7901a149ce5dd68d6352c90f07bff570ca2b4c
Koji Ishii <kojii@chromium.org>
2025-05-04 17:29:19 +0900

    [ci] Add `-n` option to `precommit.sh`

--- hooks/precommit.sh
+++ hooks/precommit.sh
@@ -1,6 +1,15 @@
-set -x
-cargo test
-# cargo fmt --all --check
-cargo fmt --all
-# cargo clippy --all-targets --all-features -- -D warnings
-cargo clippy --fix --allow-dirty --all-targets --all-features -- -D warnings
+#!/bin/bash
+set -e
+(
+  set -x
+  cargo test --all-targets --all-features
+)
+if [[ "$1" == '-n' ]]; then
+  set -x
+  cargo fmt --all --check
+  cargo clippy --all-targets --all-features -- -D warnings
+else
+  set -x
+  cargo fmt --all
+  cargo clippy --fix --allow-dirty --all-targets --all-features -- -D warnings
+fi
