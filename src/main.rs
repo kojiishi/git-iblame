ae6c1cb957254a33e8b7c6fec1cacf9564068900
Koji Ishii <kojii@chromium.org>
2025-05-12 04:32:48 +0900

    cli: Use `clap` for the command line argument parser
    Fixes #8.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
-use std::{env, path::PathBuf};
+use std::path::PathBuf;
 
+use clap::Parser;
 use git_iblame::ui::Cli;
+
+/// Interactive enhanced `git blame` command line tool.
+#[derive(Debug, Default, Parser)]
+#[command(version, about)]
+struct Args {
+    /// Path of the file to blame.
+    path: PathBuf,
+}
 
 fn main() -> anyhow::Result<()> {
     env_logger::init();
 
-    let args: Vec<String> = env::args().collect();
-    let path = PathBuf::from(&args[1]);
-    let mut cli: Cli = Cli::new(&path);
+    let options = Args::parse();
+    let mut cli: Cli = Cli::new(&options.path);
     cli.run()
 }
