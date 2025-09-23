kojiishi/git-iblame

commit 80826d78261e1cb9a6c5954649c34e1c708a971b

Show the `FileDeleted` error only once (#135)
Improves #127.

src/blame/file_content.rs
@@ -8,7 +8,7 @@ use log::*;
 
 use crate::extensions::GitTools;
 
-use super::{BlameError, DiffPart, FileCommit, FileHistory, Line, LineNumberMap};
+use super::{BlameError, DiffPart, FileCommit, FileCommits, FileHistory, Line, LineNumberMap};
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub enum ContentType {
@@ -265,35 +265,47 @@ impl FileContent {
             "apply_commits: {first_index}..{} skip={skip}",
             commits.len()
         );
-        self.apply_commits(&commits[first_index..], skip)?;
+        self.apply_commits(commits, first_index, skip)?;
         self.update_lines_after_apply();
         self.applied_commits_len = commits.len();
         trace!("update_commits done, elapsed: {:?}", start_time.elapsed());
         Ok(())
     }
 
-    fn apply_commits(&mut self, commits: &[FileCommit], skip: usize) -> anyhow::Result<()> {
-        for i in skip..commits.len() {
-            self.apply_commit(commits, i)?;
-        }
-        Ok(())
-    }
-
-    fn apply_commit(&mut self, commits: &[FileCommit], commit_index: usize) -> anyhow::Result<()> {
-        let commit = &commits[commit_index];
-        if commit_index == 0 {
-            self.apply_diff_parts(commit.diff_parts(), commit)?;
-        } else {
-            // If `commit_index > 0`, the line numbers in `commit.diff_parts().new`
-            // aren't the line numbers in `self.lines`. Map them to the line
-            // numbers of `self.lines`.
-            let mut adjusted_parts = commit.diff_parts().clone();
-            for j in (0..commit_index).rev() {
-                let parts = &commits[j].diff_parts();
-                let map = LineNumberMap::new_new_from_old(parts);
-                map.apply_to_parts(&mut adjusted_parts);
+    fn apply_commits(
+        &mut self,
+        commits: &FileCommits,
+        first_index: usize,
+        skip: usize,
+    ) -> anyhow::Result<()> {
+        // Don't apply if any earlier commits have failed to apply.
+        for commit in &commits[0..first_index + skip] {
+            if commit.is_apply_failed() {
+                trace!("is_appy_failed {}", commit.commit_id());
+                return Ok(());
             }
-            self.apply_diff_parts(&adjusted_parts, commit)?;
+        }
+        let commits = &commits[first_index..];
+        for commit_index in skip..commits.len() {
+            let commit = &commits[commit_index];
+            if commit.is_apply_failed() {
+                trace!("is_appy_failed {commit_index} {}", commit.commit_id());
+                break;
+            }
+            if commit_index == 0 {
+                self.apply_diff_parts(commit.diff_parts(), commit)?;
+            } else {
+                // If `commit_index > 0`, the line numbers in `commit.diff_parts().new`
+                // aren't the line numbers in `self.lines`. Map them to the line
+                // numbers of `self.lines`.
+                let mut adjusted_parts = commit.diff_parts().clone();
+                for j in (0..commit_index).rev() {
+                    let parts = &commits[j].diff_parts();
+                    let map = LineNumberMap::new_new_from_old(parts);
+                    map.apply_to_parts(&mut adjusted_parts);
+                }
+                self.apply_diff_parts(&adjusted_parts, commit)?;
+            }
         }
         Ok(())
     }
@@ -314,6 +326,7 @@ impl FileContent {
         trace!("apply: #{} {part:?} {}", commit.index(), commit_id);
         let new_line_numbers = part.new.line_numbers();
         if new_line_numbers.is_empty() {
+            commit.set_apply_failed();
             return self.insert_deleted_part(part, commit_id);
         }
 

Cargo.toml
@@ -1,6 +1,6 @@
 [package]
 name = "git-iblame"
-version = "0.8.6"
+version = "0.8.7"
 edition = "2024"
 rust-version = "1.89"
 authors = ["Koji Ishii <kojiishi@gmail.com>"]

src/blame/file_commit.rs
@@ -1,6 +1,7 @@
 use std::{
     io::BufReader,
     path::{Path, PathBuf},
+    sync::atomic::{self, AtomicBool},
 };
 
 use log::*;
@@ -27,6 +28,7 @@ pub struct FileCommit {
     author_email: Option<String>,
     old_path: Option<PathBuf>,
     diff_parts: Vec<DiffPart>,
+    is_apply_failed: AtomicBool,
 }
 
 impl FileCommit {
@@ -40,6 +42,7 @@ impl FileCommit {
             author_email: None,
             old_path: None,
             diff_parts: Vec::new(),
+            is_apply_failed: AtomicBool::new(false),
         }
     }
 
@@ -86,6 +89,15 @@ impl FileCommit {
 
     pub fn diff_parts(&self) -> &Vec<DiffPart> {
         &self.diff_parts
+    }
+
+    pub fn is_apply_failed(&self) -> bool {
+        self.is_apply_failed.load(atomic::Ordering::Relaxed)
+    }
+
+    pub(crate) fn set_apply_failed(&self) {
+        trace!("set_apply_failed: {self:?}");
+        self.is_apply_failed.store(true, atomic::Ordering::Relaxed);
     }
 
     pub fn read(&mut self, git: &GitTools) -> anyhow::Result<()> {

commit 8b3f8c378d51dc0d1a6d02343379efcbd686b328

Fix a regression in #135

src/blame/file_content.rs
@@ -326,8 +326,7 @@ impl FileContent {
         trace!("apply: #{} {part:?} {}", commit.index(), commit_id);
         let new_line_numbers = part.new.line_numbers();
         if new_line_numbers.is_empty() {
-            commit.set_apply_failed();
-            return self.insert_deleted_part(part, commit_id);
+            return self.insert_deleted_part(part, commit);
         }
 
         // Saturate `end`, as it may be set to `MAX`.
@@ -349,11 +348,13 @@ impl FileContent {
         Ok(())
     }
 
-    fn insert_deleted_part(&mut self, part: &DiffPart, commit_id: git2::Oid) -> anyhow::Result<()> {
+    fn insert_deleted_part(&mut self, part: &DiffPart, commit: &FileCommit) -> anyhow::Result<()> {
         trace!("insert_deleted_part: {part:?}");
+        let commit_id = commit.commit_id();
         let new_line_numbers = part.new.line_numbers();
         assert!(new_line_numbers.is_empty());
         if new_line_numbers.start == 0 {
+            commit.set_apply_failed();
             return Err(BlameError::FileDeleted(commit_id).into());
         }
         let old_line_numbers = &part.old.line_numbers;